185a54ff3fc14bf7ea3a35a49aefca76bbf9f3c9
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2022-12-31 10:50:00 +0100

    Edit ApiController to send a file stream

--- apps/api/src/api.controller.ts
+++ apps/api/src/api.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, StreamableFile, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import type { Response } from 'express';
+import { createReadStream, ReadStream } from 'fs';
+import { join } from 'path';
 import { File } from './file.entity';
 import { FileService } from './file/file.service';
 
@@ -16,7 +19,17 @@ export class ApiController {
   }
 
   @Get(":filename")
-  getFileByName(@Param('filename') filename: string): Promise<File | null> {
-    return this.fileService.getFileByName(filename);
+  async getFileByName(
+    @Res({ passthrough: true }) res: Response,
+    @Param('filename') filename: string
+    ): Promise<StreamableFile> {
+    const file: File = await this.fileService.getFileByName(filename);
+    const fileStream: ReadStream = createReadStream(join(process.cwd(), file.path));
+
+    res.set({
+      'Content-Type': file.mimetype,
+      'Content-Disposition': `attachment; filename="${file.filename}"`
+    })
+    return new StreamableFile(fileStream);
   }
 }
