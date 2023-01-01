bcf485253b7817b1de19ba81922b030198784cd1
Cyril Raffatelli <cyril.raffatelli@outlook.com>
2023-01-01 17:09:08 +0100

    Edit web app FileService

--- apps/web/src/app/file.service.ts
+++ apps/web/src/app/file.service.ts
@@ -6,7 +6,6 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class FileService {
-
   private readonly _api: string;
 
   constructor(
@@ -19,10 +18,10 @@ export class FileService {
     const formData = new FormData();
 
     formData.append("file", file, file.name);
-    return this.http.post<Express.Multer.File>(this._api + "/upload", formData);
+    return this.http.post<Express.Multer.File>(this._api + "/files", formData);
   }
 
   getFileByName(name: string): Observable<Blob> {
-    return this.http.get(this._api + `/file/${name}`, { responseType: 'blob' });
+    return this.http.get(this._api + `/files/${name}`, { responseType: 'blob' });
   }
 }
